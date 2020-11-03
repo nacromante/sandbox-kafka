321a2e6186f15f761a69bce4f49ba1d42123b533
fred <fredericond@hotmail.com>
2020-11-03 02:36:17 -0300

    Initial commit

---
+++ src/main/java/com/sandbox/sandboxkafka/SandboxKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.sandboxkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sandbox/sandboxkafka/SandboxKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.sandboxkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
